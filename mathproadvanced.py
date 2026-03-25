64e45497abcf122087baf2f8e713cd6600b71d62
davizuco01-sudo <davizuco01@gmail.com>
2026-03-25 15:21:31 +0100

    Update mathproadvanced.py

--- mathproadvanced.py
+++ mathproadvanced.py
@@ -13,7 +13,7 @@ os.environ["CREWAI_DISABLE_TELEMETRY"] = "true"
 # 1. CONFIGURACIÓN
 load_dotenv()
 # VOLVEMOS AL 70B: Ahora que no leemos PDFs gigantes, no rozaremos el límite de la API.
-mi_llm = "groq/llama3-8b-8192"
+mi_llm = "groq/llama-3.2-90b-text-preview"
 
 # 2. HERRAMIENTAS BLINDADAS
 @tool("calculadora")
